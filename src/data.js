3f33306adb85c5dfa3c5d426f6f900fe6cc4ef55
sumit-max-arch <sumitchowdhary01@gmail.com>
2021-09-19 13:02:07 +0530

    push amazon project to git hub

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'LCD TV',
+            category: 'Accessories',
+            image: './images/product-1.jpg',
+            price: 120,
+            brand: 'LG',
+            rating: 4.5,
+            numReview: 10,
+            description: 'High Quality telivision'
+        },
+        {
+            _id: '2',
+            name: 'TV LCD',
+            category: 'Accessories',
+            image: '/images/product-2.jpg',
+            price: 20,
+            brand: 'MI',
+            rating: 3.5,
+            numReview: 10,
+            description: 'Very High Quality telivision'
+        },
+        {
+            _id: '3',
+            name: 'Best Tv',
+            category: 'Accessories',
+            image: '/images/product-3.jpg',
+            price: 67,
+            brand: 'LG',
+            rating: 2.5,
+            numReview: 10,
+            description: 'Good High Quality telivision'
+        },
+        {
+            _id: '4',
+            name: 'LCD TV Tress',
+            category: 'Appliance',
+            image: '/images/product-4.jpg',
+            price: 456,
+            brand: 'Redmi',
+            rating: 4.8,
+            numReview: 10,
+            description: 'Welcome High Quality telivision'
+        },
+        {
+            _id: '5',
+            name: 'Nice TV',
+            category: 'Appliance',
+            image: '/images/product-5.jpg',
+            price: 12,
+            brand: 'Samsung',
+            rating: 4.8,
+            numReview: 7,
+            description: 'Extreme Quality telivision'
+        },
+        {
+            _id: '6',
+            name: 'NEW TV',
+            category: 'Accessories',
+            image: '/images/product-6.jpg',
+            price: 120,
+            brand: 'Iphone',
+            rating: 4.9,
+            numReview: 10,
+            description: 'Performance High Quality telivision'
+        },
+        {
+            _id: '7',
+            name: 'BAD TV',
+            category: 'Accessories',
+            image: '/images/product-7.jpg',
+            price: 1,
+            brand: 'XIAOMI',
+            rating: 1.8,
+            numReview: 3,
+            description: 'Bad Quality telivision'
+        },
+        {
+            _id: '8',
+            name: 'SUN TV',
+            category: 'Access',
+            image: '/images/product-8.jpg',
+            price: 31,
+            brand: 'REDMI',
+            rating: 3.8,
+            numReview: 3,
+            description: 'worst Quality telivision'
+        },
+    ],
+};
+
+export default data;
